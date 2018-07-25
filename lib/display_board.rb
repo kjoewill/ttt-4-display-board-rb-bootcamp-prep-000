2948f1cb8564613a8fe67abf24d16832e26e3b39
kjoewill@gmail.com <kjoewill@gmail.com>
2018-07-25 20:12:21 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -11,9 +11,9 @@ def display_board(board)
   if board.empty?
     display_empty_board
   else
-    row1 = "#{board[0]}|#{board[1]}|#{board[2]}\n"
-    row2 = "#{board[3]}|#{board[4]}|#{board[5]}\n"
-    row3 = "#{board[6]}|#{board[7]}|#{board[8]}\n"
+    row1 = " #{board[0]} | #{board[1]} | #{board[2] }\n"
+    row2 = " #{board[3]} | #{board[4]} | #{board[5]}\n"
+    row3 = " #{board[6]} | #{board[7]} | #{board[8]}\n"
   
     seperator = "-----------\n"
   
