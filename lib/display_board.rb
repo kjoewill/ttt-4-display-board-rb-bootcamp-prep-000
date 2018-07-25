b8f5e107f900601d03c513e453456cc7571545f5
kjoewill@gmail.com <kjoewill@gmail.com>
2018-07-25 20:17:58 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -12,8 +12,8 @@ def display_board(board)
     display_empty_board
   else
     row1 = " #{board[0]} | #{board[1]} | #{board[2] }\n"
-    row2 = " #{board[3]} | #{board[4]} | #{board[5]}\n"
-    row3 = " #{board[6]} | #{board[7]} | #{board[8]}\n"
+    row2 = " #{board[3]} | #{board[4]} | #{board[5] }\n"
+    row3 = " #{board[6]} | #{board[7]} | #{board[8] }\n"
   
     seperator = "-----------\n"
   
