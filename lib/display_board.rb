4e4d00cc7ce004688a4d322d49a2f07169d1f289
kjoewill@gmail.com <kjoewill@gmail.com>
2018-07-25 20:00:34 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,13 +1,14 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-def old_display_board
+def display_empty_board
   row = "   |   |   \n"
   seperator = "-----------\n"
   puts row + seperator + row + seperator + row
 end
 
 def display_board(board)
+  if 
   row1 = "#{board[0]}|#{board[1]}|#{board[2]}\n"
   row2 = "#{board[3]}|#{board[4]}|#{board[5]}\n"
   row3 = "#{board[6]}|#{board[7]}|#{board[8]}\n"
