c7b271f7faa452c660d1617d9349b74dcc20ea1e
kjoewill@gmail.com <kjoewill@gmail.com>
2018-07-25 19:51:16 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-def old_display_board(board)
+def old_display_board
+  row = "   |   |   \n"
+  seperator = "-----------\n"
+  puts row + seperator + row + seperator + row
+end
+
+def display_board(board)
   row = "   |   |   \n"
   seperator = "-----------\n"
   puts row + seperator + row + seperator + row
