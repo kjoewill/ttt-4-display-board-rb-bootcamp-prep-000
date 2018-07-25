d47ce8c6f86c28bed6fd747eff400d8e6ba5366b
kjoewill@gmail.com <kjoewill@gmail.com>
2018-07-25 20:01:18 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,7 +8,7 @@ def display_empty_board
 end
 
 def display_board(board)
-  if 
+  if board
   row1 = "#{board[0]}|#{board[1]}|#{board[2]}\n"
   row2 = "#{board[3]}|#{board[4]}|#{board[5]}\n"
   row3 = "#{board[6]}|#{board[7]}|#{board[8]}\n"
